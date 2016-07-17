e11291aede4bf9cc9c97b5b97ed239a086c2f94e
Jason Kulatunga <jk17@ualberta.ca>
2016-07-17 14:58:44 -0700

    working library.

--- docker.js
+++ docker.js
@@ -1,5 +1,85 @@
+var Hyper = require('./hyper');
+
+var hyper = new Hyper();
+hyper.listImages(function (err, containers) {
+    if(err){
+        console.log("ERROR:", err)
+        return
+    }
+
+    containers.forEach(function (containerInfo) {
+        console.log(containerInfo)
+        // docker.getContainer(containerInfo.Id).stop(cb);
+    });
+});
+
+
+// var r7kamuraSign = require('./r7kamura-signer');
 // var Docker = require('dockerode');
-// var docker = new Docker({host: 'http://us-west-1.hyper.sh', port: 443});
+// var Modem = require('docker-modem')
+// var request_options = {
+//     //body: '',
+//     headers: {
+//         // "X-Hyper-Date": new Date().toISOString(),
+//         Host: 'us-west-1.hyper.sh:443',
+//         "Content-Type": "application/json"
+//
+//     },
+//     method: 'GET',
+//     uri: 'https://us-west-1.hyper.sh/v1.23/containers/json?all=1'
+// };
+//
+//
+// var sign = new r7kamuraSign({
+//     accessKeyId: process.env.HYPER_ACCESS_KEY,
+//     body: request_options.body || '',
+//     headers: request_options.headers,
+//     method: request_options.method,
+//     region: 'us-west-1',
+//     service: 'hyper',
+//     secretAccessKey: process.env.HYPER_SECRET_KEY,
+//     url: request_options.uri
+// });
+// request_options.headers.Authorization = sign.toString();
+//
+// console.log(request_options)
+//
+//
+// // //override the Modem prototype
+// // // Modem.prototype.buildRequest = function(options, context, data, callback) {
+// // //     console.log(arguments)
+// // // }
+// //
+// // var modem = new Modem({
+// //     host: 'us-west-1.hyper.sh',
+// //     port: 443,
+// //     protocol: 'https'
+// // });
+// //
+// // modem.dial({
+// //     path: '/v1.23/containers/json?',
+// //     method:'GET',
+// //     headers: request_options.headers,
+// //     statusCodes:{200:'success allowed'},
+// //     options: {'all':'1'}
+// //
+// // }, function(){
+// //     console.log(arguments)
+// // })
+//
+//
+// Modem.prototype.dial = function(options, callback){
+//
+//     console.log("DIAL:",options)
+//     console.log(this.host)
+// }
+//
+// //
+// // Modem.prototype.buildRequest = function(options, context, data, callback) {
+// //     console.log(arguments)
+// // }
+//
+// var docker = new Docker({protocol:'https', host: 'us-west-1.hyper.sh', port: 443});
 // docker.listContainers(function (err, containers) {
 //     if(err){
 //         console.log("ERROR:", err)
@@ -10,65 +90,3 @@
 //         docker.getContainer(containerInfo.Id).stop(cb);
 //     });
 // });
-
-var r7kamuraSign = require('./r7kamura-signer');
-var Modem = require('docker-modem');
-var request = require('request')
-
-
-
-
-var request_options = {
-    //body: '',
-    headers: {
-        // "X-Hyper-Date": new Date().toISOString(),
-        Host: 'us-west-1.hyper.sh:443',
-        "Content-Type": "application/json"
-
-    },
-    method: 'GET',
-    uri: 'https://us-west-1.hyper.sh/v1.23/containers/json?all=1'
-};
-
-
-var sign = new r7kamuraSign({
-    accessKeyId: process.env.HYPER_ACCESS_KEY,
-    body: request_options.body || '',
-    headers: request_options.headers,
-    method: request_options.method,
-    region: 'us-west-1',
-    service: 'hyper',
-    secretAccessKey: process.env.HYPER_SECRET_KEY,
-    url: request_options.uri
-});
-request_options.headers.Authorization = sign.toString();
-// request(request_options, function (error, response, body) {
-//     // Do more stuff with 'body' here
-//     // console.log(error);
-//     // console.log(response)
-//     // console.log(body)
-// });
-console.log(request_options)
-
-
-//override the Modem prototype
-// Modem.prototype.buildRequest = function(options, context, data, callback) {
-//     console.log(arguments)
-// }
-
-var modem = new Modem({
-    host: 'us-west-1.hyper.sh',
-    port: 443,
-    protocol: 'https'
-});
-
-modem.dial({
-    path: '/v1.23/containers/json?',
-    method:'GET',
-    headers: request_options.headers,
-    statusCodes:{200:'success allowed'},
-    options: {'all':'1'}
-
-}, function(){
-    console.log(arguments)
-})
---
+++ hyper.js
@@ -0,0 +1,58 @@
+var Docker = require('dockerode'),
+    Modem = require('docker-modem'),
+    Signer = require('./r7kamura-signer'),
+    util = require('util'),
+    url = require('url');
+
+var _config = null;
+
+//Override the Modem methods;
+var orig_Modem_prototype_buildRequest = Modem.prototype.buildRequest;
+
+Modem.prototype.buildRequest = function(options, context, data, callback) {
+
+    //generate the url from the modem & _config options
+    var request_url = url.format({
+        protocol: _config.protocol,
+        slashes: true,
+        hostname: options.hostname,
+        port: options.port,
+        pathname: options.path
+    });
+
+    // options.headers["Content-Type"] = "application/json";
+    var sign = new Signer({
+        accessKeyId: _config.access_key,
+        body: options.file || '',
+        headers: options.headers,
+        method: options.method,
+        region: 'us-west-1',
+        service: 'hyper',
+        secretAccessKey: _config.secret_key,
+        url: request_url
+    });
+    options.headers.Authorization = sign.toString();
+
+    orig_Modem_prototype_buildRequest.call(this, options, context, data, callback);
+};
+
+
+//Create a new Hyper constructor with default options and security keys
+function Hyper(opts) {
+    if(!opts){
+        opts = {};
+    }
+    _config = opts;
+    opts.protocol = opts.protocol || 'https';
+    opts.host = opts.host || 'us-west-1.hyper.sh';
+    opts.port = opts.port || 443;
+    opts.access_key = opts.access_key || process.env.HYPER_ACCESS_KEY;
+    opts.secret_key = opts.secret_key || process.env.HYPER_SECRET_KEY;
+    opts.version = 'v1.23';
+
+    Hyper.super_.call(this, opts);
+}
+
+util.inherits(Hyper, Docker);
+
+module.exports = Hyper;
